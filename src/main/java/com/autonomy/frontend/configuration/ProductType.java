5978f88959846637544e49e7c67df8473e8b6f97
Alex Scown <alex.scown@hp.com>
2013-12-03 12:20:12 +0000

    [frontend/configuration] Merge bugfixes back into trunk. [rev: luca.mandrioli]
    [git-p4: depot-paths = "//depot/products/api/frontend/configuration/trunk/configuration-api/": change = 1088685]

---
+++ src/main/java/com/autonomy/frontend/configuration/ProductType.java
@@ -0,0 +1,25 @@
+package com.autonomy.frontend.configuration;
+
+/*
+ * $Id:$
+ *
+ * Copyright (c) 2013, Autonomy Systems Ltd.
+ *
+ * Last modified by $Author:$ on $Date:$
+ */
+public enum ProductType {
+
+    AXE("Content"),
+    SERVICECOORDINATOR("Coordinator"),
+    UASERVER("Community");
+
+    private final String friendlyName;
+
+    private ProductType(final String friendlyName) {
+        this.friendlyName = friendlyName;
+    }
+
+    public String getFriendlyName() {
+        return friendlyName;
+    }
+}
