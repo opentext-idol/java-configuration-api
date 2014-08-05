8096219ec42e3fe4ed323b4db139567d0177e2e7
Alex Scown <alex.scown@hp.com>
2014-08-05 15:32:38 +0000

    [configuration] Implement BCryptUsernameAndPassword and related login support. Bring in LoginTypes from login library. [rev: alyona.ivanova]
    [git-p4: depot-paths = "//depot/products/api/frontend/configuration/trunk/configuration-api/": change = 1168694]

---
+++ src/main/java/com/autonomy/frontend/configuration/LoginTypes.java
@@ -0,0 +1,17 @@
+package com.autonomy.frontend.configuration;
+
+/*
+ * $Id:$
+ *
+ * Copyright (c) 2014, Autonomy Systems Ltd.
+ *
+ * Last modified by $Author:$ on $Date:$
+ */
+public class LoginTypes {
+
+    public static final String DEFAULT = "default";
+    public static final String CAS = "cas";
+    public static final String EXTERNAL = "external";
+    public static final String SINGLE_USER = "singleUser";
+
+}
